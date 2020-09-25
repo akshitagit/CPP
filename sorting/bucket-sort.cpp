54e4d06960f09323e3af6327fada4252e4cb24c3
Jabbar Memon <jabbar@zoop.one>
2020-09-25 10:57:37 +0530

    Added bucket Sort

--- sorting/bucket-sort.cpp
+++ sorting/bucket-sort.cpp
@@ -1,42 +1,40 @@
-// C++ program to sort an array using bucket sort 
-#include <algorithm> 
-#include <iostream> 
-#include <vector> 
-using namespace std; 
-
-// Function to sort arr[] of size n using bucket sort 
-void bucketSort(float arr[], int n) 
-{ 
-	// 1) Create n empty buckets 
-	vector<float> b[n]; 
-
-	// 2) Put array elements in different buckets 
-	for (int i = 0; i < n; i++) { 
-		int bi = n * arr[i]; // Index in bucket 
-		b[bi].push_back(arr[i]); 
-	} 
-
-	// 3) Sort individual buckets 
-	for (int i = 0; i < n; i++) 
-		sort(b[i].begin(), b[i].end()); 
-
-	// 4) Concatenate all buckets into arr[] 
-	int index = 0; 
-	for (int i = 0; i < n; i++) 
-		for (int j = 0; j < b[i].size(); j++) 
-			arr[index++] = b[i][j]; 
-} 
-
-/* Driver program to test above function */
-int main() 
-{ 
-	float arr[] = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 }; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
-	bucketSort(arr, n); 
-
-	cout << "Sorted array is \n"; 
-	for (int i = 0; i < n; i++) 
-		cout << arr[i] << " "; 
-	return 0; 
-} 
-
+#include <iostream>
+#include <algorithm>
+#include <vector> //used for the sake of simplicity
+using namespace std;
+ 
+ 
+void bucketSort(float arr[], int n)
+{
+    vector<float> b[n];
+    
+    for (int i=0; i<n; i++)
+    {
+       int x = n*arr[i];
+       b[x].push_back(arr[i]);
+    }
+ 
+    for (int i=0; i<n; i++)
+       sort(b[i].begin(), b[i].end());
+ 
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < b[i].size(); j++)
+          arr[index++] = b[i][j];
+}
+ 
+int main()
+{
+    float arr[] = {0.235, 0.101, 0.476, 0.7645, 0.15, 0.142};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Before Sorting\n";
+    for (int i=0; i<n; i++)
+    cout << arr[i] << " ";
+    
+    bucketSort(arr, n);
+    
+    cout << "\nAfter Sorting \n";
+    for (int i=0; i<n; i++)
+       cout << arr[i] << " ";
+    return 0;
+}
