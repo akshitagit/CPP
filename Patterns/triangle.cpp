b8d95c367af157c89b56a6472402dfdab108a0e6
Jayadev V <jayadevmarath@gmail.com>
2020-09-25 09:22:34 +0530

    Create triangle.cpp
    completed first half

---
+++ Patterns/triangle.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int i,j,k,l,n;    
+cout<<"Enter the Range=";    
+cin>>j;
+
+for(n = j;n>=0;n--)
+{
+for(k=0;k<=j-n;k++)
+{
+    cout<<" "
+}
+for(i=n;i>=0;i--)
+{
+cout<<i;
+}
+for(i=1;i<=n;i++)
+{
+    cout<<i;
+}
+
+cout<<"\n";
+
+}
+
+return 0;  
+}
