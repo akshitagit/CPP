40769dd63bc697a887d11824cfbe06fea41fbe94
Niharika <niharikacplyt@gmail.com>
2020-09-25 13:37:30 +0530

    added sieve of eratosthenes

--- Maths/sieve_of_erastothenes.cpp
+++ Maths/sieve_of_erastothenes.cpp
@@ -1,30 +1,41 @@
-/*
-Implementation of Sieve of Erastothenes - The most efficient way to find prime numbers upto n or check if a number is prime or not :)
+/* Sieve of Eratosthenes*/
+
+/*Algorithm to find all digits from 1 to n which are prime:
+
+1.Create a list of all digits from 2 to n which are unmarked initially.
+2.For all numbers (m) from 2 to sqrt(n) (where n is unmarked):
+	Mark all the numbers which are multiple of m and greater than m^2.
+3.Iterate through the list and print all unmarked digits.
 
 */
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-vector<int> sieve(){
-    int n=1e6+1;
-    vector<int> counts(n,1);
-    for(int i=2;i*i<=n;i++){
-        
-        if(counts[i]==1){
-            for(int j=i*i;j*j<=n;j+=i){
-                counts[j]=0;
-            }
+int main()
+{
+    vector<int>v;
+    int n; //Enter the digit
+    cout<<"Enter the value of n:";
+	cin>>n;
+    if(n<2)
+        {
+            cout<<"No prime numbers found.";
+            return 0;
         }
-    }
-    return(counts);
-}
 
-int main(){
-    cout<<"Enter the numer to check if its prime or not: ";
-    int num;
-    cin>>num;
-    if(sieve()[num]) cout<<"Yes";
-    else cout<<"No";
-    return(0);
+    for (int a = 0; a <= n; a++)
+		v.push_back(1); //Initially unmarking all the digits
+
+	for (int m = 2; m <= sqrt(n); m++) //Iterating from 2 to sqrt(n)
+	{
+		if (v[m] == 1)
+		{
+			for (int a = 2; m * a <= n; a++)
+				v.at(a*m)= 0; //Marking all digits which are multiple of m and greater than m^2
+		}
+	}
+    cout<<"The prime numbers between 2 and "<< n <<" are: "<<" ";
+	for (int i=2;i<=n;i++)
+		if (v[i] == 1)
+			cout<< i << " ";
+
 }
