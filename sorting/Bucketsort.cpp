cac89730824bb8b5fb72ce92f7d69d4057b348a2
Mayankgupta-coder <65400711+Mayankgupta-coder@users.noreply.github.com>
2020-10-02 22:46:09 +0530

    Add Bucket sort

---
+++ sorting/Bucketsort.cpp
@@ -0,0 +1,51 @@
+// C++ program to sort an array using bucket sort 
+#include <algorithm> 
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+// Function to sort arr[] of size n using bucket sort 
+void bucketSort(float arr[], int n) 
+{ 
+	// 1) Create n empty buckets 
+	vector<float> b[n]; 
+
+	// 2) Put array elements in different buckets 
+	for (int i = 0; i < n; i++) { 
+		int bi = n * arr[i]; // Index in bucket 
+		b[bi].push_back(arr[i]); 
+	} 
+
+	// 3) Sort individual buckets 
+	for (int i = 0; i < n; i++) 
+		sort(b[i].begin(), b[i].end()); 
+
+	// 4) Concatenate all buckets into arr[] 
+	int index = 0; 
+	for (int i = 0; i < n; i++) 
+		for (int j = 0; j < b[i].size(); j++) 
+			arr[index++] = b[i][j]; 
+} 
+
+/* Driver program to test above function */
+int main() 
+{ 
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        float arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        bucketSort(arr, n); 
+
+    	cout << "Sorted array is \n"; 
+    	for (int i = 0; i < n; i++) 
+    		cout << arr[i] << " "; 
+    }
+	
+	return 0; 
+} 
