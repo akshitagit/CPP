8f54b8d75c8d8c54b37104eddfa1b1ec9e5910d1
B Gayathri <69475106+bvsslgayathri-8679@users.noreply.github.com>
2020-10-21 21:46:40 +0530

    Heart pattern in cpp

---
+++ Patterns/heart_pattern.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,a,b;
+    for(j=0;j<3;j++)
+    {
+        for(i=1;i<=20;i++)
+        {
+            if(i>=4-j && i<=6+j ||i>=15-j &&i<=17+j)
+            cout<<"*";
+            else
+            {
+                cout<<" ";
+            }
+            
+            
+        }
+        cout<<endl;
+        //lowerpart
+    }
+    for(a=1;a<=11;a++)
+    {
+        for(b=1;b<=20;b++)
+        {
+        if(b>=a-1&&b<=21-a)
+        cout<<"*";
+        else
+        {
+            cout<<" ";
+        }
+        }
+    cout<<endl;  
+    }
+        
+}
+
