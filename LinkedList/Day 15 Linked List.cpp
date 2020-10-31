f97168d78adc22db4fb2b54221883bc4014fa019
Mandar Desai <66965756+msdesai1499@users.noreply.github.com>
2020-10-31 20:14:13 +0530

    Linked List Prg

---
+++ LinkedList/Day 15 Linked List.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+      	
+          Node *temp=new Node(data);
+          if(head==NULL)
+          {
+              head=temp;
+          }
+          else
+          {
+            Node *curr;
+            curr=head;
+            while(curr->next!=NULL)
+            {
+                curr=curr->next;
+            }
+            curr->next=temp;
+
+          }
+          return head;
+          //Complete this method
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
