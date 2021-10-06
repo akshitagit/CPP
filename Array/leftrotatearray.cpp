db38a1aa1effdd0cae44441ceef939cd43a7669a
Sunita Singh <sunitasinghaer@gmail.com>
2021-10-06 20:04:36 +0530

    Update leftrotatearray.cpp

--- Array/leftrotatearray.cpp
+++ Array/leftrotatearray.cpp
@@ -1,37 +1,19 @@
+// C++ program to implement left rotation in an array.
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverse(int arr[], int low, int high)
-{
-  for (int i = low, j = high; i < j; i++, j--)
-  {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-}
-
-void leftRotate(int arr[], int r, int n)
-{
-  reverse(arr, 0, r - 1);
-
-  reverse(arr, r, n - 1);
-
-  reverse(arr, 0, n - 1);
-}
-
 int main()
 {
-  int n;
-  cin >> n;
-  int a[n];
-  for (int i = 0; i < n; i++)
-  {
-    cin >> a[i];
-  }
-
-  int r = 3;
-  leftRotate(a, r, n);
-
+    int n;
+    int k;
+    cin >> n >> k;
+    int a[n];
+    for(int i = 0;i < n;i++)
+        cin >> a[i];
+    for(int i=k;i<n;i++)
+        cout<<a[i]<<" ";
+    for(int i=0;i<k;i++)
+        cout<<a[i]<<" ";
   return 0;
 }
