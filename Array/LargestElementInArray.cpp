6b62e5591fb936a88284347c7a499620516136e4
shivam kumar <75497119+shivam-kumar123@users.noreply.github.com>
2021-10-07 11:22:45 +0530

    it is not a good practice to change array
    I have taken another variable maxi and founded the maximum of the array in a maxi variable. 
    time complexity: O(N)
    Space Complexity: O(1)

--- Array/LargestElementInArray.cpp
+++ Array/LargestElementInArray.cpp
@@ -17,14 +17,15 @@ int main()
        cin >> arr[i];
     }
 
-    // Loop to store largest number to arr[0]
-    for(i = 1;i < n; ++i)
+    // Loop to store largest number to maxi
+    long long int maxi = 0;
+    for(i = 0;i < n; ++i)
     {
        // Change < to > if you want to find the smallest element
-       if(arr[0] < arr[i])
-           arr[0] = arr[i];
+       if(maxi < arr[i])
+           maxi = arr[i];
     }
-    cout << "Largest element = " << arr[0];
+    cout << "Largest element = " << maxi;
 
     return 0;
     }
