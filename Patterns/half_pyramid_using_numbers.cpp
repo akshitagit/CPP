7ab30a8901371303beb3ca993856aad03ebea386
Shivansh Dwivedi <dwivediayush2002@gmail.com>
2022-10-08 23:04:41 +0530

    Create half_pyramid_using_numbers.cpp
    Creating a half pyramid using numbers

---
+++ Patterns/half_pyramid_using_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << "\n";
+    }
+}
