1b307a835be898bc5b5f4591e5c4c881340c2bea
Jyotirmoy <97742841+intozen@users.noreply.github.com>
2022-10-16 01:56:44 +0530

    Add files via upload

---
+++ Sorting&Searching/Find_min_and_max_elem_in_an_array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    ll mn = 1e18, mx = -1;
+    for (int i = 0; i < n; i++) {
+        mn = min(a[i], mn);
+        mx = max(a[i], mx);
+    }
+    return {mn, mx};
+}
